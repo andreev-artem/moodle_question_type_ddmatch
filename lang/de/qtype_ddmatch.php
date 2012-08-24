7d6db7cee3283a2194f0510855dc7d42bc44eb1d
Lea Haensenberger <lea.haensenberger@gmail.com>
2012-08-24 08:21:48 +0200

    Adding german translation

--- lang/de/qtype_ddmatch.php
+++ lang/de/qtype_ddmatch.php
@@ -1,4 +1,4 @@
-<?PHP // $Id: qtype_ddmatch.php,v 1.1 2010/06/21 16:12:24 aaboyd Exp $ 
+<?PHP // $Id: qtype_ddmatch.php,v 1.1 2010/06/21 16:12:24 aaboyd Exp $
       // qtype_ddmatch.php - created with Moodle 1.9 + (Build: 20080402) (2007101509)
 
 
@@ -7,5 +7,7 @@ $string['ddmatch'] = 'Drag-and-Drop-Zuordnung';
 $string['draganswerhere'] = 'Ziehen Sie mit der Maus die Antwort an die richtige Stelle in diesem Bereich';
 $string['editingddmatch'] = 'Drag-and-Drop Zuordnungsfrage bearbeiten';
 $string['ddmatchsummary'] = 'Eine Erweiterung der Zuordnungsfrage, die dem Benutzer ermöglicht, Antworte per Drag-and-Drop zuzuordnen.';
+$string['pluginnameadding'] = 'Eine Drag-and-Drop Zuordnungsfrage hinzufügen';
+$string['pluginnameediting'] = 'Eine Drag-and-Drop Zuordnungsfrage bearbeiten';
 
 ?>
